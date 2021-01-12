d6e2da448829be21fb9f4094ca9a3cf95ef22673
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-12 15:46:15 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+string result;
+srand(time(0));
+int i = rand()%9;
+string grade[9]={"A","B+","B","C+","C","D+","D","F","W"};
+cout <<"Press Enter 3 times to reveal your future.";
+cin.get();
+cin.get();
+cin.get();
+
+if(i==0) result = grade[0];
+else if (i==1) result = grade[1];
+else if (i==2) result = grade[2];
+else if (i==3) result = grade[3];
+else if (i==4) result = grade[4];
+else if (i==5) result = grade[5];
+else if (i==6) result = grade[6];
+else if (i==7) result = grade[7];
+else if (i==8) result = grade[8];
+
+cout<<"You will get "<<result<<" in this 261102.";
+
+}
